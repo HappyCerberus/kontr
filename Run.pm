5ee196aecc1c2629e6eb4c6b83c9bfc17b2b715b
Tomas Brukner <xbrukner@fi.muni.cz>
2012-03-23 21:14:01 +0100

    Added option to change limit of output from UnitTest.

--- Run.pm
+++ Run.pm
@@ -23,6 +23,8 @@ around 'exec' => sub
 
 	if ($unit_test->session->got_value('run_timeout'))
 	{ $self->limit_runtime($unit_test->session->get_value('run_timeout')); }
+	if ($unit_test->session->got_value('run_output'))
+	{ $self->limit_output($unit_test->session->get_value('run_output')); }
 
 	$self->work_path($unit_test->work_path);
 	$self->cmd($unit_test->work_path."/".$unit_test->name);
