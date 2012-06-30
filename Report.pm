38e23a165f44a5a19ca491a33d523a98da5efb23
Tomas Brukner <xbrukner@fi.muni.cz>
2012-06-30 23:52:32 +0200

    First version of Report for points and tags.

---
+++ Report.pm
@@ -0,0 +1,47 @@
+#  Submission.pm
+#  
+#  Copyright 2012 Tomáš Brukner <xbrukner@fi.muni.cz>
+#  
+# Licensed under the MIT lincense:
+# http://www.opensource.org/licenses/mit-license.php
+
+package Report;
+
+#use MasterTest;
+#use UnitTest;
+use Moose;
+use Moose::Util::TypeConstraints;
+
+#has 'master' => ( is => 'ro', isa => 'MasterTest', required => 1 );
+#has 'unit' => ( is => 'ro', isa => 'UnitTest', required => 1 );
+has 'master' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'unit' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'subtest' => ( is => 'rw', isa => 'Str', predicate => 'has_subtest' );
+has 'tags' => ( traits => ['Array'], is => 'rw', isa => 'ArrayRef[Str]', default => sub { [] }, handles => { addTag => 'push', allTags => 'elements' } );
+has 'points' => ( traits => ['Hash'], is => 'rw', isa => 'HashRef[Num]', default => sub { {} }, handles => { addPoints => 'set', allPoints => 'kv' } );
+
+sub summary {
+	my $self = shift;
+	
+	#$self->master->name.'/'.$self->unit->name.($self->has_subtest ? '/'.$self->subtest : '').': '.
+	$self->master.'/'.$self->unit.($self->has_subtest ? '/'.$self->subtest : '').': '.
+	join(' ', $self->allTags).'; '.join(' ', map { $_->[0].'='.$_->[1] } $self->allPoints );
+}
+
+sub sumPoints {
+	my %res;
+	my $data = shift;
+		
+	use Data::Dumper;
+	
+	foreach my $report ( @$data ) {
+		foreach ( $report->allPoints ) {
+			if (exists $res{$_->[0]} ) { $res{$_->[0]} += $_->[1]; }
+			else { $res{$_->[0]} += $_->[1]; }
+		}
+	}
+	return %res;
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
